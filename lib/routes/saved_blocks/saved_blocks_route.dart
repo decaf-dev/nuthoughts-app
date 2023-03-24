99afbd4811eb7eed261a8f18f35f2624e9101cac
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-03-24 01:46:50 -0600

    feat: add saved blocks list

--- lib/routes/saved_blocks/saved_blocks_route.dart
+++ lib/routes/saved_blocks/saved_blocks_route.dart
@@ -1,6 +1,6 @@
+import 'package:chisel/controllers/app_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
 
 class SavedBlocksRoute extends StatefulWidget {
   const SavedBlocksRoute({super.key});
@@ -10,13 +10,29 @@ class SavedBlocksRoute extends StatefulWidget {
 }
 
 class _SavedBlocksRouteState extends State<SavedBlocksRoute> {
+  final AppController controller = Get.find();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Recent Thoughts'),
+        title: const Text('Recently Saved'),
       ),
-      body: Placeholder(),
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(25),
+        child: ListView.builder(
+          itemCount: controller.savedBlocks.length,
+          itemBuilder: (context, index) {
+            return Card(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Text(
+                        controller.savedBlocks.reversed.toList()[index].text)));
+          },
+        ),
+      )),
     );
   }
 }
