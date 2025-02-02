e189567f89b3d00848c14a17c7dc3925b4fa1313
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2025-02-02 13:35:11 -0700

    refactor: rename colors to meaningful names

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,35 +1,39 @@
 import 'package:flutter/material.dart';
 
-const Color white2 = Color(0xfff4f6f8);
-const Color darkBlue = Color(0xff35364b);
-const Color lightGray = Color(0xfff0f0f0);
-const Color gray2 = Color(0xffbbbdbe);
+class LightColors {
+  static Color white100 = Color(0xfff4f6f8);
+  static Color white200 = Color(0xfff0f0f0);
+  static Color gray400 = Color(0xffbbbdbe);
+  static Color blue900 = Color(0xff35364b);
+}
 
 final ThemeData theme = ThemeData(
   useMaterial3: true,
-  colorScheme: const ColorScheme.light(
+  colorScheme: ColorScheme.light(
       primary: Colors.white,
-      onPrimary: darkBlue,
-      secondary: white2,
-      onSecondary: darkBlue,
+      onPrimary: LightColors.blue900,
+      secondary: LightColors.white100,
+      onSecondary: LightColors.blue900,
       tertiary: Colors.blue,
       onTertiary: Colors.white,
-      outline: lightGray),
+      outline: LightColors.white200),
 );
 
-const Color darkBlue2 = Color(0xff1c232e);
-const Color darkerBlue2 = Color(0xff11191f);
-const Color darkGray = Color(0xff4f5457);
-const Color borderGray = Color(0xff3a4147);
+class DarkColors {
+  static Color blue600 = Color(0xff4f5457);
+  static Color blue700 = Color(0xff3a4147);
+  static Color blue800 = Color(0xff1c232e);
+  static Color blue900 = Color(0xff11191f);
+}
 
 final ThemeData darkTheme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.dark(
-      primary: darkBlue2,
+      primary: DarkColors.blue800,
       onPrimary: Colors.white,
-      secondary: darkBlue2,
-      onSecondary: darkGray,
+      secondary: DarkColors.blue800,
+      onSecondary: DarkColors.blue600,
       tertiary: Colors.blue,
       onTertiary: Colors.white,
-      outline: borderGray),
+      outline: DarkColors.blue700),
 );
